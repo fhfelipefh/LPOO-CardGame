cc51a308626e354784acf0596ac49858a7221b03
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2025-05-05 11:21:16 -0400

    Create Carta_.java

---
+++ target/generated-sources/annotations/br/edu/seuprojeto/lpoo/cardgame/model/Carta_.java
@@ -0,0 +1,19 @@
+package br.edu.seuprojeto.lpoo.cardgame.model;
+
+import br.edu.seuprojeto.lpoo.cardgame.model.Categoria;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-05-05T11:20:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Carta.class)
+public class Carta_ { 
+
+    public static volatile SingularAttribute<Carta, Integer> ataque;
+    public static volatile SingularAttribute<Carta, Integer> raridade;
+    public static volatile SingularAttribute<Carta, Categoria> categoria;
+    public static volatile SingularAttribute<Carta, String> nome;
+    public static volatile SingularAttribute<Carta, Integer> id;
+    public static volatile SingularAttribute<Carta, Integer> defesa;
+
+}
