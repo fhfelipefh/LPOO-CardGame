59678d18a2f417ae5e67f9f5de875ae2872a4f1d
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2025-05-12 10:41:52 -0300

    Aumento de tamanho do form

--- src/main/java/br/edu/seuprojeto/lpoo/cardgame/view/CartaJF.java
+++ src/main/java/br/edu/seuprojeto/lpoo/cardgame/view/CartaJF.java
@@ -32,11 +32,11 @@ public class CartaJF extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addGap(0, 1054, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 434, Short.MAX_VALUE)
+            .addGap(0, 559, Short.MAX_VALUE)
         );
 
         pack();
