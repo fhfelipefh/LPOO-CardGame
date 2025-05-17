7b0a2f17f2d9bc8938fd40981de39b4e286000a7
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2025-05-17 17:23:48 -0300

    Refatoração de classes e objetos

--- src/main/java/br/edu/seuprojeto/lpoo/cardgame/control/InterfaceBD.java
+++
@@ -1,9 +0,0 @@
-package br.edu.seuprojeto.lpoo.cardgame.control;
-
-public interface InterfaceBD {
-    public Boolean conexaoAberta();
-    public void fecharConexao();
-    public Object find(Class c, Object id) throws Exception;
-    public void persist(Object o) throws Exception;
-    public void remover(Object o) throws Exception;
-}
