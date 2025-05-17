fb0a12cd26d47c14539d1c70ea0dca1947e02fed
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2025-05-17 17:30:02 -0300

    Update Jogador.java

--- src/main/java/model/Jogador.java
+++ src/main/java/model/Jogador.java
@@ -12,6 +12,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -26,11 +29,15 @@ public class Jogador implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "jog_id")
     private int id;
+
     @Column(name = "jog_nickname", nullable = false)
     private String nickname;
+
     @Column(name = "jog_level")
     private int level;
     
+    @ManyToMany
+    @JoinTable(name = "tbl_jogador_baralho", joinColumns = @JoinColumn(name = "jog_id"), inverseJoinColumns = @JoinColumn(name = "car_id"))
     private List<Carta> baralho;
 
     public Jogador() {
